7d1d1a2c616d25711fab73513029c35d51b9acfb
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-03-31 16:26:11 +0530

    React Features Project is completed

--- ReactJs-BobZiroll/React-Features/src/App.jsx
+++ ReactJs-BobZiroll/React-Features/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import reactHalfLogo from "./assets/reacthalf.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="container">
+      <header>
+        <a href="#" className="logo-button">
+          <img src={reactLogo} className="logo" alt="React logo" />
+          ReactFacts
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>React Course-Project1</nav>
+      </header>
+
+      <div className="content">
+        <main>
+          <h1>Fun facts about React</h1>
+          <ul>
+            <li>Was first Release in 2013</li>
+            <li>Was Originally created by Jorden Walker</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Power Thousands of enterprise apps,including mobile apps</li>
+          </ul>
+        </main>
+        <div className="react-image">
+          <img
+            src={reactHalfLogo}
+            className="react-half-logo"
+            alt="React Half Logo"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
